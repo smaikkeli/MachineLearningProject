83e229cf121236e4b2db75532d09967067a863d3
DLcw <75663692+dianalaucw@users.noreply.github.com>
2022-12-11 14:43:10 +0200

    Update featureSelection.R

--- featureSelection.R
+++ featureSelection.R
@@ -9,7 +9,7 @@ read_process <- function(filename,name){
 }
 res_all<-read_process('res_all','All features')
 res_correclated<-read_process('res_correlated','Highly correclated')
-res_exCorreclated<-read_process('res_exCorrelated','Excluding highly correclated')
+res_exCorreclated<-read_process('res_exCorrelated','less correclated')
 res_lasso<-read_process('res_lasso','Lasso')
 res_lsvc<-read_process('res_lsvc','lsvc')
 #res_RF<-read_process('res_RF','Random Forest')
@@ -17,21 +17,31 @@ res_lsvc<-read_process('res_lsvc','lsvc')
 t<-melt(dplyr::full_join(res_all,dplyr::full_join(res_correclated,dplyr::full_join(res_exCorreclated,dplyr::full_join(res_lasso,res_lsvc)))))
 
 testmse<-ggplot(data = t[t$variable=='test_mse',],aes(x=Approach,y=value,group=Models,color=Models))+
-  geom_line()+labs(title='Test squared error',fill='Models')+theme(legend.title = element_text(size=15),
+  geom_line(linewidth=1.5)+labs(title='Test squared error',fill='Models')+theme(legend.title = element_text(size=15),
                                                                    legend.text = element_text(size=15),
                                                                    legend.key.size = unit(1, 'cm'),
                                                                    axis.text.y = element_text(colour = 'black', size = 12), 
-                                                                   axis.title.y = element_text(size = 20, 
-                                                                                               hjust = 0.5, vjust = 0.2),
-                                                                   axis.text.x = element_text(colour = 'black', size = 12), 
-                                                                   axis.title.x = element_text(size = 20, 
-                                                                                               hjust = 0.5, vjust = 0.2))
+                                                                   axis.title.y = element_blank(),
+                                                                   axis.text.x =  element_text(colour = 'black', size = 12), 
+                                                                   axis.title.x=element_blank())
 ggsave(file='testmse.png',path='/home/laucheuk/Desktop/DATA11002intro_to_ML/project',width=10,height=5)
 
 csvmse<-ggplot(data = t[t$variable=='csv_mse',],aes(x=Approach,y=value,group=Models,color=Models))+
-  geom_line()+labs(title='CV mean squared error',fill='Models')
+  geom_line(linewidth=1.5)+labs(title='CV mean squared error',fill='Models')+theme(legend.title = element_text(size=15),
+                                                                      legend.text = element_text(size=15),
+                                                                      legend.key.size = unit(1, 'cm'),
+                                                                      axis.text.y = element_text(colour = 'black', size = 12), 
+                                                                      axis.title.y = element_blank(),
+                                                                      axis.text.x =  element_text(colour = 'black', size = 12), 
+                                                                      axis.title.x=element_blank())
 ggsave(file='csvmse.png',path='/home/laucheuk/Desktop/DATA11002intro_to_ML/project',width=10,height=5)
 
 accuracy<-ggplot(data = t[t$variable=='accuracy',],aes(x=Approach,y=value,group=Models,color=Models))+
-  geom_line()+labs(title='Accuracy',fill='Models')
+  geom_line(linewidth=1.5)+labs(title='Accuracy',fill='Models')+theme(legend.title = element_text(size=15),
+                                                         legend.text = element_text(size=15),
+                                                         legend.key.size = unit(1, 'cm'),
+                                                         axis.text.y = element_text(colour = 'black', size = 12), 
+                                                         axis.title.y = element_blank(),
+                                                         axis.text.x =  element_text(colour = 'black', size = 12), 
+                                                         axis.title.x=element_blank())
 ggsave(file='acc.png',path='/home/laucheuk/Desktop/DATA11002intro_to_ML/project',width=10,height=5)
